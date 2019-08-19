50d6de12165b4774a97b5ba6efcb797509120d4b
Ophélie Le Vigouroux <levigouroux.oph@gmail.com>
2019-08-20 00:00:11 +0200

    ajax : refresh picture/quote

--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,65 @@
-
-console.log("index.js")
-
-
+//----------------------------------------------------------
+//--------------- Affichage --------------------------------
+//----------------------------------------------------------
+/**
+ * Afficher la citation
+ */
+const picture = document.querySelector('#picture')
 const quote = document.querySelector('#quote')
-setTimeout(function(){
-    quote.classList.add('is-visible')
-}, 2200)
-console.log(quote)
+setTimeout(() => quote.classList.add('is-visible'), 2200)
 
+//----------------------------------------------------------
+//--------------- Boutons ----------------------------------
+//----------------------------------------------------------
+const url = '127.0.0.1:8080'
 
-// fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', {
-//     method: 'GET',
-//     body: null,
-// })
-// .then((response) => response.json())
-// .then(data => console.log(data))
-// .catch(error => console.log(error));
+/**
+ * Bouton recharger l'image
+ */
+document.querySelector('#refresh-picture').addEventListener('click', event => {
+    event.preventDefault()        
+    apiRefresh('/api/refresh-picture').then(
+        response => {
+            const picture = document.querySelector('#picture')
+            picture.src = response.largeImageURL
+            picture.alt = response.tags
+        },
+        error => console.log(error)
+    )
+})
+
+/**
+ * Bouton recharger la citation
+ */
+document.querySelector('#refresh-quote').addEventListener('click', event => {
+    event.preventDefault()
+    apiRefresh('/api/refresh-quote').then(
+        response => {
+            document.querySelector('#quote-text').innerHTML = response.quoteText
+            document.querySelector('#quote-author').innerHTML = response.quoteAuthor
+        },
+        error => console.log(error)
+    )
+})
+
+//----------------------------------------------------------
+//--------------- API --------------------------------------
+//----------------------------------------------------------
+/**
+ * 
+ * @param {*} url 
+ */
+const apiRefresh = (url) => {
+    const xhr = new XMLHttpRequest()
+    return new Promise( (resolve, reject) => {
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                const data = JSON.parse(xhr.responseText)
+                resolve(data)
+            }
+        }
+        xhr.open("GET", url, true)
+        xhr.send()
+        // reject("")
+    })
+}
--- server.js
+++ server.js
@@ -5,21 +5,44 @@ const app = express();
 
 app.get('/', async function(req, res) {
     const image = await (new apiPicture()).getPicture()
-    const quote = await (new apiQuote()).getQuote()
-    
     if(!image){
         res.status(424).send('Image not found');
     }
-
+    
+    const quote = await (new apiQuote()).getQuote()
     if(!quote){
         res.status(424).send('Quote not found');
     }
-  
+    
     res.render('./index.ejs', {
         image: image,
         quote: quote
     })
 });
+
+app.get('/api/refresh-picture', async function(req, res) {
+    const image = await (new apiPicture()).getPicture()
+    if(!image){
+        res.status(424).send('Image not found');
+    }
+
+    res.json({
+        largeImageURL: image.largeImageURL,
+        tags: image.tags
+    })
+})
+
+app.get('/api/refresh-quote', async function(req, res) {
+    const quote = await (new apiQuote()).getQuote()
+    if(!quote){
+        res.status(424).send('Quote not found');
+    }
+
+    res.json({
+        quoteAuthor: quote.quoteAuthor,
+        quoteText: quote.quoteText
+    })
+})
 
 app.use('/public', express.static('public'));
 
@@ -29,21 +52,3 @@ app.use(function(req, res, next){
 });
 
 app.listen(8080);
-
-// const url = require('url');
-
-// app.get('/sous-sol', function(req, res) {
-//     const page = url.parse(req.url).pathname;
-//     console.log("*********", page);
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.send('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
-// });
-
-// app.get('/etage/1/chambre', function(req, res) {
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.send('Hé ho, c\'est privé ici !');
-// });
-
-// app.get('/etage/:etagenum/chambre', function(req, res) {
-//     res.render('../views/chambres.ejs', {etage: req.params.etagenum});
-// });
